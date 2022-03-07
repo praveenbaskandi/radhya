d4cb403ee079e00b9e353279ac95283d0dc7f8eb
Praveen Baskandi <praveenbaskandi@Praveens-MacBook-Pro.local>
2022-03-08 00:22:29 +0530

    Updates

--- src/constants/index.js
+++
@@ -1,7 +0,0 @@
-export const SOCIAL_LINK = {
-    FACEBOOK: "https://www.facebook.com/",
-    INSTAGRAM: "https://www.instagram.com/redhya_dherya/",
-    LINKDIN: "https://www.linkdin.com/",
-    YOUTUBE: "https://www.youtube.com/",
-    TWITTER: "https://www.twitter.com/",
-  };
--- src/pages/Page3/index.js
+++
@@ -1,8 +0,0 @@
-
-const Page3 = () => (
-        <div style={{height: '500px', width: "100%" }} className='content'>
-            <span className="img-txt">a trip to Space</span>
-        </div>
-);
-
-export default Page3;
